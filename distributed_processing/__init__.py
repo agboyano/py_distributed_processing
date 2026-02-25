f192121e5ce90758360f1e78ed6454ba6d76fa13
agboyano <agboyano@protonmail.com>
2026-02-25 18:13:24 +0100

    logging set to ERROR by default

--- distributed_processing/__init__.py
+++ distributed_processing/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.0.1a"
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-logging.getLogger(__name__).setLevel(logging.INFO)
+logging.getLogger(__name__).setLevel(logging.ERROR)
